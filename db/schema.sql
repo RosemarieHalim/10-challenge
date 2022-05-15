56cd994a60b01c024c520b6664e340d54b8112dc
Rosemarie <rosemarie.m.halim@gmail.com>
2022-05-15 05:55:20 -0400

    created seeds for tables

--- db/schema.sql
+++ db/schema.sql
@@ -12,17 +12,18 @@ CREATE TABLE employees (
   id INTEGER AUTO_INCREMENT PRIMARY KEY,
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
-  party_id INTEGER,
-  industry_connected BOOLEAN NOT NULL,
-  CONSTRAINT fk_party FOREIGN KEY (party_id) REFERENCES parties(id) ON DELETE SET NULL
+  role_id INTEGER,
+  employee_connected BOOLEAN NOT NULL,
+  CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES roles(id) ON DELETE SET NULL
 );
 
 CREATE TABLE roles (
   id INTEGER AUTO_INCREMENT PRIMARY KEY,
-  voter_id INTEGER NOT NULL,
-  candidate_id INTEGER NOT NULL,
+  title VARCHAR(30) NOT NULL,
+  salary VARCHAR(30) NOT NULL,
+  department_id INTEGER NOT NULL,
   created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
-  CONSTRAINT uc_voter UNIQUE (voter_id),
-  CONSTRAINT fk_voter FOREIGN KEY (voter_id) REFERENCES voters(id) ON DELETE CASCADE,
+  CONSTRAINT uc_voter UNIQUE (employee_id),
+  CONSTRAINT fk_voter FOREIGN KEY (employee_id) REFERENCES voters(id) ON DELETE CASCADE,
   CONSTRAINT fk_candidate FOREIGN KEY (candidate_id) REFERENCES candidates(id) ON DELETE CASCADE
 );
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,21 +1,30 @@
-INSERT INTO departments (name, description)
+INSERT INTO departments (name)
 VALUES
- ('Git Gurus', 'Need to resolve a merge conflict? The Git Gurus have your back. Nobody knows Git like these folks do.');
+ ('IT'),
+ ('Accounting'),
+ ('Operations'),
+ ('Marketing');
 
-INSERT INTO employees (first_name, last_name, party_id, industry_connected)
+INSERT INTO employees (first_name, last_name, role_id, manager_id)
 VALUES
   ('Ronald', 'Firbank', 1, 1),
-  ('Virginia', 'Woolf', 1, 1),
+  ('Virginia', 'Woolf', 3, NULL),
+  ('Jack', 'London', 2, NULL),
+  ('Peter', 'Greenway', 4, 3),
+  ('Derek', 'Jarman', 5, NULL),
+  ('ALice', 'Davis', 8, 7),
+  ('Jen', 'Ferny', 7, NULL),
+  ('Callen', 'Willis', 6, 5),
+  ('Robert', 'Defalen', 7, NULL),
+  ('Sandy', 'Powell', 1, NULL);
 
-INSERT INTO roles (first_name, last_name, email)
+INSERT INTO roles (title, salary, department_id)
 VALUES
-  ('James', 'Fraser', 'jf@goldenbough.edu'),
-  ('Jack', 'London', 'jlondon@ualaska.edu'),
-  ('Robert', 'Bruce', 'rbruce@scotland.net'),
-  ('Peter', 'Greenaway', 'pgreenaway@postmodern.com'),
-  ('Derek', 'Jarman', 'djarman@prospectcottage.net'),
-  ('Paolo', 'Pasolini', 'ppasolini@salo.com'),
-  ('Heathcote', 'Williams', 'hwilliams@bafta.com'),
-  ('Sandy', 'Powell', 'spowell@oscars.com'),
-  ('Emil', 'Zola', 'ezola@requin.com'),
-  ('Sissy', 'Coalpits', 'scoalpits@greenaway.com'),
+  ('Developer', '80000', '1'),
+  ('Software Engineer', '90000', '1'),
+  ('Accountant', 'Fraser', '100000'),
+  ('Analyst', 'Fraser', '125000'),
+  ('Project Manager', '60000', '3'),
+  ('OM', 'Fraser', '75000'),
+  ('Graphic Designer', '50000', '4'),
+  ('Art Director', '90000', '4');
