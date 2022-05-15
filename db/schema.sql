79f69818042f2cc9f26ec6415a6dda6af084a83a
Rosemarie <rosemarie.m.halim@gmail.com>
2022-05-15 04:57:05 -0400

    added base reference code from module

---
+++ db/schema.sql
@@ -0,0 +1,28 @@
+DROP TABLE IF EXISTS departments;
+DROP TABLE IF EXISTS employees;
+DROP TABLE IF EXISTS roles;
+
+CREATE TABLE departments (
+  id INTEGER AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(50) NOT NULL,
+  description TEXT
+);
+
+CREATE TABLE employees (
+  id INTEGER AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  party_id INTEGER,
+  industry_connected BOOLEAN NOT NULL,
+  CONSTRAINT fk_party FOREIGN KEY (party_id) REFERENCES parties(id) ON DELETE SET NULL
+);
+
+CREATE TABLE roles (
+  id INTEGER AUTO_INCREMENT PRIMARY KEY,
+  voter_id INTEGER NOT NULL,
+  candidate_id INTEGER NOT NULL,
+  created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+  CONSTRAINT uc_voter UNIQUE (voter_id),
+  CONSTRAINT fk_voter FOREIGN KEY (voter_id) REFERENCES voters(id) ON DELETE CASCADE,
+  CONSTRAINT fk_candidate FOREIGN KEY (candidate_id) REFERENCES candidates(id) ON DELETE CASCADE
+);
---
+++ db/seeds.sql
@@ -0,0 +1,21 @@
+INSERT INTO departments (name, description)
+VALUES
+ ('Git Gurus', 'Need to resolve a merge conflict? The Git Gurus have your back. Nobody knows Git like these folks do.');
+
+INSERT INTO employees (first_name, last_name, party_id, industry_connected)
+VALUES
+  ('Ronald', 'Firbank', 1, 1),
+  ('Virginia', 'Woolf', 1, 1),
+
+INSERT INTO roles (first_name, last_name, email)
+VALUES
+  ('James', 'Fraser', 'jf@goldenbough.edu'),
+  ('Jack', 'London', 'jlondon@ualaska.edu'),
+  ('Robert', 'Bruce', 'rbruce@scotland.net'),
+  ('Peter', 'Greenaway', 'pgreenaway@postmodern.com'),
+  ('Derek', 'Jarman', 'djarman@prospectcottage.net'),
+  ('Paolo', 'Pasolini', 'ppasolini@salo.com'),
+  ('Heathcote', 'Williams', 'hwilliams@bafta.com'),
+  ('Sandy', 'Powell', 'spowell@oscars.com'),
+  ('Emil', 'Zola', 'ezola@requin.com'),
+  ('Sissy', 'Coalpits', 'scoalpits@greenaway.com'),
